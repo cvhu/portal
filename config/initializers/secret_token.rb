e4f533ba1841803e8070da1d0718ede2cdc00155
Vic Hu <vic@cvhu.org>
2012-02-12 02:43:32 -0600

    first commit for the rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portal::Application.config.secret_token = '52d9af46ae813345b2bd4270eb9e6b47d12f38b380ca61053b335983a2d9eccdf819543e5dd0c8819677af5d8d63a89f87a96c51daa6decfad0411d968911816'
